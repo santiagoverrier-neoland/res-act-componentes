e14a60f6e3f352526b36390f06e093e03bb12690
Santi Acosta Verrier <santiav@gmail.com>
2026-06-15 20:22:04 -0300

    Clase lunes 15 junio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Hace que le ponga los estilos responsivos automáticamente a las imágenes
+    image: {
+        responsiveStyles: true,
+    },
+});
